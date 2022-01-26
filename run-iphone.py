03b019dfbe9b4627dfeb90ec952964ba2730314f
gurustacks <40200122+gurustacks@users.noreply.github.com>
2022-01-26 20:31:18 +0100

    updates

--- run-iphone.py
+++ run-iphone.py
@@ -24,27 +24,30 @@ def run(playwright: Playwright) -> None:
         proxy = get_proxy()
         proxy_test = {"server": f"http://{proxy['ip']}:{proxy['port']}", "username": f"{proxy['username']}", "password": f"{proxy['password']}"}
         print(proxy_test)
-        browser = playwright.firefox.launch(headless=False)
-        context = browser.new_context(proxy={"server": f"http://{proxy['ip']}:{proxy['port']}", "username": f"{proxy['username']}", "password": f"{proxy['password']}"})
-        # Open new page
+        
+        webkit = playwright.webkit
+        iphone = playwright.devices["iPhone 12"]
+        browser = webkit.launch(headless=False, proxy={"server": f"http://{proxy['ip']}:{proxy['port']}", "username": f"{proxy['username']}", "password": f"{proxy['password']}"})
+        context = browser.new_context(**iphone)
         page = context.new_page()
+
         stealth_sync(page)
         # Go to https://coinmarketcap.com/
         page.route("**/*.svg", lambda route: route.abort()) 
         page.route("**/*.png", lambda route: route.abort()) 
         page.route("**/*.jpg", lambda route: route.abort())
         page.route("**/*.woff2", lambda route: route.abort())
-        page.route("**/inpage.js", lambda route: route.abort())
+        #page.route("**/inpage.js", lambda route: route.abort())
         
         entry_pages = ["https://coinmarketcap.com/newsletter/","https://coinmarketcap.com/newsletter/"]
         
         chosen_page = random.choice(entry_pages)
         print(chosen_page)
-        page.goto(chosen_page, timeout=50000, wait_until="networkidle")
-        
+        page.goto(chosen_page, timeout=50000)
+        page.click("#__next > div > div.main-content > div.bywovg-0.kuGegY > div:nth-child(2) > div > nav > div > div:nth-child(2) > svg") 
         # Click text=Search
         #page.click("#__next > div > div.main-content > div.bywovg-0.kuGegY > div:nth-child(1) > div > div.sc-111rrsy-0.qbrWo > div:nth-child(6) > div > div.sc-266vnq-1.gffsPR")
-        page.click("text=Search")
+        #page.click("text=Search")
         # Fill [placeholder="What\ are\ you\ looking\ for\?"]
         page.fill("[placeholder=\"What\\ are\\ you\\ looking\\ for\\?\"]", "MANA\n")
         # Click text=Decentraland
@@ -52,14 +55,16 @@ def run(playwright: Playwright) -> None:
         #with page.expect_navigation():
         page.click("text=Decentraland")
         # assert page.url == "https://coinmarketcap.com/currencies/decentraland/"
+      
         page.evaluate(
             """
             var intervalID = setInterval(function () {
-                scroll(0,1000);
+                scroll(0,4000);
             }, 200);
 
             """
         )
+      
         # Click text=👍 Good
         page.click("text=👍 Good")
         # Click text=×
